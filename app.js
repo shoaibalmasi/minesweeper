e269e31464063acde38701f3dadf2764e6624a6b
shoaibalmasi <shoaibalmasi@gmail.com>
2020-08-17 12:27:07 +0430

    change server setting

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
-const express = require("express");
-const app = express();
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
+var indexRouter = require('./routes/index');
 
-app.use(express.urlencoded({
-  extended: false
-}));
-app.use(express.text());
-app.use(express.static('public'));
+var app = express();
 
-// 
-app.get('/', (req , res) =>{
-    res.sendFile(__dirname+'/views/minesweeper.html')
-})
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-// get not found page
-app.get('*', function (req, res) {
-  res.status(404).send('not found');
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.listen(8000, ()=>{
-    console.log('server is listening');
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+module.exports = app;
